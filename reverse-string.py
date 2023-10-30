3f37ab0ea49748d16ec633b4809b2ec2e4bb3d1a
Shariq <shariqnadeem21@hotmail.com>
2023-10-30 20:12:39 +0800

    A reverse string question

---
+++ reverse-string.py
@@ -0,0 +1,18 @@
+'''
+The question was sourced from a newsletter 
+This question is asked by Google. Given a string, reverse all of its characters and return the resulting string.
+
+Ex: Given the following strings...
+
+“Cat”, return “taC”
+“The Daily Byte”, return "etyB yliaD ehT”
+“civic”, return “civic”
+
+'''
+
+def reverse_string(string):
+    reversed_str = [i for i in string]
+    rev_str = ""
+    for i in range(len(reversed_str)):
+        rev_str += reversed_str[(len(reversed_str)-1)-i]
+    return rev_str
